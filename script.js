d6fb745b6ca96ce58c103763a956820571fa80e4
summekoushal <2020bcs003@sggs.ac.in>
2023-07-11 09:53:01 +0530

    added logo'

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-let btn = document.getElementById('submit');
-btn.addEventListener('click', function(e) {
-    e.preventDefault()
-    let firstname = document.getElementById("firstname").value;
-    let lastname = document.getElementById("lastname").value;
-    let email = document.getElementById("email").value;
-    let subject = document.getElementById("subject").value;
-    let textarea = document.getElementById("textarea").value;
+// let btn = document.getElementById('submit');
+// btn.addEventListener('click', function(e) {
+//     e.preventDefault()
+//     let firstname = document.getElementById("firstname").value;
+//     let lastname = document.getElementById("lastname").value;
+//     let email = document.getElementById("email").value;
+//     let subject = document.getElementById("subject").value;
+//     let textarea = document.getElementById("textarea").value;
 
-    let body = firstname +
-        "<br/>" +
-        lastname +
-        "<br/>" +
-        email +
-        "<br/>" +
-        subject +
-        "<br/>" +
-        textarea;
-    
-    Email.send({
-      SecureToken: "845ae5c9-9f8d-4cd6-8864-27124d5a5835",
-      To: "2020bcs003@sggs.ac.in",
-      From: "2020bcs003@sggs.ac.in",
-      Subject: "Contact message",
-      Body:body
-    }).then((message) => alert(message));
+//     let body = firstname +
+//         "<br/>" +
+//         lastname +
+//         "<br/>" +
+//         email +
+//         "<br/>" +
+//         subject +
+//         "<br/>" +
+//         textarea;
 
-})
+//     Email.send({
+//       SecureToken: "845ae5c9-9f8d-4cd6-8864-27124d5a5835",
+//       To: "2020bcs003@sggs.ac.in",
+//       From: "2020bcs003@sggs.ac.in",
+//       Subject: "Contact message",
+//       Body:body
+//     }).then((message) => alert(message));
+
+// })
+// width="640" height="350"
