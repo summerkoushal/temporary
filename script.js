f258e88728775e3c666f7923972a87a3eba6b792
summekoushal <2020bcs003@sggs.ac.in>
2023-07-12 01:56:42 +0530

    sdlkf

--- script.js
+++ script.js
@@ -27,3 +27,13 @@
 
 // })
 // width="640" height="350"
+ var prevScrollpos = window.pageYOffset;
+ window.onscroll = function () {
+   var currentScrollPos = window.pageYOffset;
+   if (prevScrollpos > currentScrollPos) {
+     document.getElementById("navbar").style.top = "0";
+   } else {
+     document.getElementById("navbar").style.top = "-60px";
+   }
+   prevScrollpos = currentScrollPos;
+ };
